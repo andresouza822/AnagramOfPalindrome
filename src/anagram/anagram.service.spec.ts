f6dfd8f3cd59d9d066f1ca4eab8403e2177a1a33
souzaandre <andre.souza822@gmail.com>
2019-05-29 23:53:54 -0300

    Cria Anagram Service Spec

---
+++ src/anagram/anagram.service.spec.ts
@@ -0,0 +1,54 @@
+import AnagramService from "./anagram.service.ts";
+
+
+describe("Anagram Service", function() {
+    
+    let anagramService = new AnagramService();
+    let anagramsArray = [
+        'escolaalocse',
+        'es!colaalocse',
+        'subinoonibus',
+        'testeetset',
+        'ttseesteet',
+        'santanderrednatnas',
+        'qualquercoisa',
+    ];
+
+    afterAll(function() {
+        console.log('anagramsArray tested:',anagramsArray);
+    });
+
+
+    it("should validate anagramsArray to have more than one anagram of palindrome invalid", function() {
+        
+        let invalidAnagrams = anagramsArray.filter(anagram => anagramService.isAnagramOfPalindrome(anagram))
+        expect(
+            invalidAnagrams.length
+        ).toBeGreaterThan(1);
+    });
+     
+    it("should not validate anagram with upper case characters", function() {
+        
+        expect(
+            anagramService.isAnagramOfPalindrome('Kaykak')
+        ).toBe(0);
+
+    });
+
+    it("should validate anagram only  with  lower case characters ", function() {
+        
+        expect(
+            anagramService.isAnagramOfPalindrome('kaykak')
+        ).toBe(1);
+
+    });
+
+    it("should not accept characters out of range a-z", function() {
+        
+        expect(
+            anagramService.isAnagramOfPalindrome('escola2alocse')
+        ).toBe(0);
+        
+    });
+
+});
