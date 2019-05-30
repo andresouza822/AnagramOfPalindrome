ff20e790d90eb42ff973db5b5445249e9178b91e
souzaandre <andre.souza822@gmail.com>
2019-05-29 22:23:15 -0300

    Corrige AnagramService
    Implementa validação de caracteres

--- src/anagram/anagram.service.ts
+++ src/anagram/anagram.service.ts
@@ -2,29 +2,35 @@
 
 export default class AnagramService{
 
-    isAnagramOfPalindrome(anagram:string){
+    private isInvalidCharacters(characters:string):boolean{
+        return characters.match(/[^a-z]+/g)?true:false;
+    }
+
+    isAnagramOfPalindrome(anagram:string):number{
+
+        if(this.isInvalidCharacters(anagram)) return 0;
 
         let evenAnagram = anagram.length % 2 == 0;
         let hasUniqueLetter = false;
 
         for(let letter of anagram.split('')){
 
-            var letterOcurrence = anagram.split(letter).length-1;
+            let letterOcurrence = anagram.split(letter).length-1;
             
             if(letterOcurrence % 2 != 0){
                 
-                if(evenAnagram) return false;
+                if(evenAnagram) return 0;
 
                 else if(letterOcurrence == 1){
                     
-                    if(hasUniqueLetter ) return false;
+                    if(hasUniqueLetter ) return 0;
                     
                     hasUniqueLetter = true;
                 }
             }
         };
 
-        return true;
+        return 1;
     }
 
 
